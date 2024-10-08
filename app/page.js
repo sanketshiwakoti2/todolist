71046fda0e8005cda1bf0501185b8bfcf4d79157
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-10-08 23:46:49 +0545

    todolist work done

--- app/page.js
+++ app/page.js
@@ -1,101 +1,89 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
 
-export default function Home() {
+const page = () => {
+  const [time, settime] = useState("");
+  const [desc, setDesc] = useState("");
+  const [task, setTask] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTask([...task, { time, desc }]);
+    settime("");
+    setDesc("");
+  };
+  let renderTask =
+    task.length == 0 ? (
+      <tr>
+        <td colSpan="2">No Task Available</td>
+      </tr>
+    ) : (
+      task.map((t, i) => {
+        return (
+          <tr className="border-2 p-4" key={i}>
+            <td className="p-4">{t.time}</td>
+            <td className="p-4">{t.desc}</td>
+            <td className="p-4">
+              <button
+                className="bg-red-500 rounded p-2 text-white"
+                onClick={(i) => {
+                  let temp=[...task]
+                  temp.splice(i,1)
+                  setTask(temp)
+                }}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        );
+      })
+    );
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <h1 className="bg-gray-800 text-white text-5xl p-4 text-center font-bold">
+        My ToDo List
+      </h1>
+      <form className="p-10 text-center" onSubmit={submitHandler}>
+        <input
+          className="border-2 border-zinc-600 text-center rounded-2xl px-3 py-2 mx-3 my-2"
+          placeholder="Enter time"
+          value={time}
+          onChange={(e) => {
+            settime(e.target.value);
+          }}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <input
+          className="border-2 border-zinc-600 text-center rounded-2xl px-3 py-2 mx-3 my-2"
+          placeholder="Enter Description"
+          value={desc}
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}
+        />
+        <button
+          className="bg-green-500 rounded-lg px-3 py-2 mx-3 font-bold"
+          type="submit"
+        >
+          Add Task
+        </button>
+      </form>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <hr />
+      <div className="text-center border">
+        <table className="border-2 border-collapse relative w-full">
+          <thead className="relative">
+            <tr>
+              <th className="p-4">time</th>
+              <th className="p-4">Description</th>
+            </tr>
+          </thead>
+          <tbody className="relative">{renderTask}</tbody>
+        </table>
+      </div>
+    </>
   );
-}
+};
+
+export default page;
